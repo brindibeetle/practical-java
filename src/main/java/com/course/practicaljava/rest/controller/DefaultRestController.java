37baf6ee3235f22ae1f39f05229d976f6b9f050f
Emile <brindi.beetle@gmail.com>
2019-08-30 13:33:23 +0200

    Add log to DefaultRestController
    Change System.out.println tot Spring log

--- src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
@@ -2,6 +2,8 @@ package com.course.practicaljava.rest.controller;
 
 import java.time.LocalTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DefaultRestController {
 
+	private Logger log = LoggerFactory.getLogger(DefaultRestController.class);
+	
 	@GetMapping("/welcome")
 	public String welcome() {
+		log.info("PIPO");
+		
 		return "Hoi, welcome!!";
 	}
 
